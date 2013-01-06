3feb18a9c71a842fadcebe1340d5a7c454aa7149
lava <lava@gruss.cc>
2013-01-06 04:31:57 +0000

    recover function

---
+++ recover.php
@@ -0,0 +1,58 @@
+<?php
+function page_recover()
+{
+$id = -1;
+if (isset($_GET["id"]))
+{
+  $id = intval($_GET["id"]);
+}
+else
+{
+  echo '<div class="slot_error" id="slot_error">FEHLER: Buchung nicht gefunden.</div><br /><br /><br />';
+  return;
+}
+$bid = -1;
+if (isset($_GET["bid"]))
+{
+  $bid = intval($_GET["bid"]);
+}
+else
+{
+  echo '<div class="slot_error" id="slot_error">FEHLER: Buchung nicht gefunden.</div><br /><br /><br />';
+  return;
+}
+$doit = false;
+if (isset($_GET["doit"]))
+{
+  $doit = true;
+}
+
+if ($doit)
+{
+echo "<h1>Buchung Nr. $id / Beleg Nr. $bid wiederhergestellt!</h1>";
+
+$query = "UPDATE vouchers SET deleted = false WHERE voucher_id = $id AND id = $bid;";
+$result = pg_query($query) or die('Abfrage fehlgeschlagen: ' . pg_last_error());
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+}
+pg_free_result($result);
+}
+else
+{
+echo '
+<div class="wiki motd">BEACHTE: Das Wiederherstellen von Buchungen bzw. Belegen kann dazu führen dass Buchungen doppelt im System sind! Benutze diese Funktion nur wenn du dir ganz sicher bist dass deine Buchung überschrieben wurde und daher diese alte Buchung wiederherstellen möchtest!</div>
+<br /><br /><br />
+<center>
+<form action="index.php" method="GET">
+<input type="hidden" name="action" value="recover" />
+<input type="hidden" name="id" value="'.$id.'" />
+<input type="hidden" name="bid" value="'.$bid.'" />
+<input type="submit" name="doit" value="Ja, ich weiß was ich tue!" />
+</form>
+</center>
+<br /><br /><br />
+';
+echo '<br /><br /><br />';
+}
+}
+?>
